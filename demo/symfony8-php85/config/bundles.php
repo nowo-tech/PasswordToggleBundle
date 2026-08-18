0527e5a76f13ee9660fe34ef419e4fb616ff89a5
Héctor Franco <hectorfranco@nowo.tech>
2026-08-18 11:24:33 +0200

    Bump demo HotReloadBundle to ^1.4 and enable FrankenPHP hot reload.

--- demo/symfony8-php85/config/bundles.php
+++ demo/symfony8-php85/config/bundles.php
@@ -7,6 +7,7 @@ return [
     Symfony\UX\Icons\UXIconsBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Nowo\PasswordToggleBundle\NowoPasswordToggleBundle::class => ['all' => true],
+    Nowo\HotReloadBundle\NowoHotReloadBundle::class => ['dev' => true, 'test' => true],
     Nowo\TwigInspectorBundle\NowoTwigInspectorBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
 ];
